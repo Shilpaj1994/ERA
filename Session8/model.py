963926d72ecb3ce3e92e8944f62583b6f75ec0a5
Shilpaj <shilpajbhalerao@gmail.com>
2023-06-30 10:41:31 +0530

    Feat: Update model class

--- Session8/model.py
+++ Session8/model.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 # -------------------------------- Session-8 --------------------------------
 class Session8(nn.Module):
     """
-    Model for Session-8 CIFAR10 dataset
+    Model for Session-9 CIFAR10 dataset
     """
     def __init__(self, normalization='batch'):
         """
@@ -19,118 +19,42 @@ class Session8(nn.Module):
         # Initialize the Module class
         super(Session8, self).__init__()
 
-        dropout_value = 0.1
+        # Dropout value of 10%
+        self.dropout_value = 0.1
 
         # Convolutional Block-1
-        self.conv_block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=20, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(20),
-            nn.Dropout(dropout_value)
-        )
-        self.conv_block2 = nn.Sequential(
-            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(20),
-            nn.Dropout(dropout_value)
-        )
+        self.conv_block1 = self.standard_conv_layer(in_channels=3, out_channels=20, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block2 = self.standard_conv_layer(in_channels=20, out_channels=20, kernel_size=3, padding=1, normalization=normalization)
 
         # Transitional Block-1
-        self.conv_point1 = nn.Sequential(
-            nn.Conv2d(in_channels=20, out_channels=16, kernel_size=1, bias=False, padding=0)
-        )
+        self.conv_point1 = self.standard_conv_layer(in_channels=20, out_channels=16, kernel_size=1, padding=0, normalization=normalization)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Convolutional Block-2
-        self.conv_block3 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=26, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(26),
-            nn.Dropout(dropout_value)
-        )
-        self.conv_block4 = nn.Sequential(
-            nn.Conv2d(in_channels=26, out_channels=26, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(26),
-            nn.Dropout(dropout_value)
-        )
-        self.conv_block5 = nn.Sequential(
-            nn.Conv2d(in_channels=26, out_channels=26, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(26),
-            nn.Dropout(dropout_value)
-        )
+        self.conv_block3 = self.standard_conv_layer(in_channels=16, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block4 = self.standard_conv_layer(in_channels=26, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block5 = self.standard_conv_layer(in_channels=26, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
 
         # Transitional Block-2
-        self.conv_point2 = nn.Sequential(
-            nn.Conv2d(in_channels=26, out_channels=16, kernel_size=1, bias=False, padding=0)
-        )
+        self.conv_point2 = self.standard_conv_layer(in_channels=26, out_channels=16, kernel_size=1, padding=0, normalization=normalization)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Convolutional Block-3
-        self.conv_block7 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(dropout_value)
-        )
-        self.conv_block8 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(dropout_value)
-        )
-        self.conv_block9 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, bias=False, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(dropout_value)
-        )
+        self.conv_block7 = self.standard_conv_layer(in_channels=16, out_channels=32, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block8 = self.standard_conv_layer(in_channels=32, out_channels=32, kernel_size=3, padding=2, normalization=normalization)
+        self.conv_block9 = self.standard_conv_layer(in_channels=32, out_channels=32, kernel_size=3, padding=1, normalization=normalization)
 
         # Global Average Pooling
-        self.gap = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1)
-        )
+        self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1))
 
         # Output Layer
-        self.conv_block10 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=1, bias=False, padding=0),
-            # nn.ReLU(),
-            # nn.BatchNorm2d(32),
-            # nn.Dropout(dropout_value)
-        )
-
-        # Batch Normalization
-        if normalization == 'batch':
-            self.norm1 = nn.BatchNorm2d(16)
-            self.norm2 = nn.BatchNorm2d(32)
-            self.norm_p1 = nn.BatchNorm2d(16)
-            self.norm3 = nn.BatchNorm2d(20)
-            self.norm4 = nn.BatchNorm2d(24)
-            self.norm5 = nn.BatchNorm2d(32)
-            self.norm_p2 = nn.BatchNorm2d(16)
-        elif normalization == 'layer':
-            self.norm1 = nn.GroupNorm(1, 16)
-            self.norm2 = nn.GroupNorm(1, 32)
-            self.norm_p1 = nn.GroupNorm(1, 16)
-            self.norm3 = nn.GroupNorm(1, 20)
-            self.norm4 = nn.GroupNorm(1, 24)
-            self.norm5 = nn.GroupNorm(1, 32)
-            self.norm_p2 = nn.GroupNorm(1, 16)
-        elif normalization == 'group':
-            self.norm1 = nn.GroupNorm(2, 16)
-            self.norm2 = nn.GroupNorm(2, 32)
-            self.norm_p1 = nn.GroupNorm(2, 16)
-            self.norm3 = nn.GroupNorm(2, 20)
-            self.norm4 = nn.GroupNorm(2, 24)
-            self.norm5 = nn.GroupNorm(2, 32)
-            self.norm_p2 = nn.GroupNorm(2, 16)
+        self.conv_block10 = self.standard_conv_layer(in_channels=32, out_channels=10, kernel_size=1, padding=0, last_layer=True, normalization=normalization)
 
     def forward(self, x):
         """
         Forward pass for model training
         :param x: Input layer
-        :return: Output of the model
+        :return: Model Prediction
         """
         # Convolutional Block-1
         x = self.conv_block1(x)
@@ -159,6 +83,44 @@ class Session8(nn.Module):
 
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=1)
+
+    def standard_conv_layer(self, in_channels: int,
+                            out_channels: int,
+                            kernel_size: int = 3,
+                            padding: int = 0,
+                            stride: int = 1,
+                            normalization: str = "batch",
+                            last_layer: bool = False):
+        """
+        Method to return a standard convolution block
+        :param in_channels: Number of input channels
+        :param out_channels: Number of output channels
+        :param kernel_size: Size of the kernel used in the layer
+        :param padding: Padding used in the layer
+        :param stride: Stride used for convolution
+        :param normalization: Type of normalization technique used
+        :param last_layer: Flag to indicate if the layer is last convolutional layer of the network
+        """
+        # Select normalization type
+        if normalization == "layer":
+            _norm_layer = nn.GroupNorm(1, out_channels)
+        elif normalization == "group":
+            if not self.group:
+                raise ValueError("Value of group is not defined")
+            _norm_layer = nn.GroupNorm(self.groups, out_channels)
+        else:
+            _norm_layer = nn.BatchNorm2d(out_channels)
+
+        # For last layer only return the convolution output
+        if last_layer:
+            return nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, kernel_size=kernel_size, bias=False, padding=padding))
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, kernel_size=kernel_size, bias=False, padding=padding),
+            nn.ReLU(),
+            _norm_layer,
+            nn.Dropout(self.dropout_value)
+        )
+
 
 # -------------------------------- Session-7 --------------------------------
 class Session7_1(nn.Module):
