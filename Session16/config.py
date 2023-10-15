d1fe3775aff40a1e6dbea9b61c050712baf7c249
Shilpaj <shilpajbhalerao@gmail.com>
2023-10-15 20:07:37 +0530

    Feat: Change the language to French

--- Session16/config.py
+++ Session16/config.py
@@ -20,7 +20,7 @@ def get_config() -> dict:
         'd_model': 512,                           # Dimension of embeddings i.e. an array of 512 values in which some represent score for living, non-living, finance, nature, etc.
                                                   # Currently, this number is around 10_000 for GPT-4
         'lang_src': "en",                         # Source language: English
-        'lang_tgt': "it",                         # Target language: Italian
+        'lang_tgt': "fr",                         # Target language: Italian
         'model_folder': "weights",                # Folder in which weights will be stored
         'model_basename': 'tmodel_',              # Prefix
         'preload': False,                         # If True, it will start from existing saved model
