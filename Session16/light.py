5174e7a545a61bd1bd9db7f7031b1e929e2fb75e
Shilpaj <shilpajbhalerao@gmail.com>
2023-10-16 22:22:29 +0530

    Debug: Added dynamic padding

--- Session16/light.py
+++ Session16/light.py
@@ -274,4 +274,4 @@ class LITTransformer(pl.LightningModule):
         """
         Method to return the DataLoader for the Validation set
         """
-        return DataLoader(self.val_ds, batch_size=1, shuffle=True)
+        return DataLoader(self.val_ds, batch_size=1, shuffle=True, collate_fn=lambda batch: collate_batch(batch))
