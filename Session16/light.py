4e09e21655006e3f34a412effa49e820c777f85e
Shilpaj <shilpajbhalerao@gmail.com>
2023-10-18 07:40:54 +0530

    Debug: Dynamic Padding issue

--- Session16/light.py
+++ Session16/light.py
@@ -45,6 +45,7 @@ class LITTransformer(pl.LightningModule):
         # Prepare Data and Model
         self.config = config
         self.prepare_data()
+        self.setup()
         self.tokenizer_src = get_or_build_tokenizer(self.config, self.ds_raw, self.config['lang_src'])
         self.tokenizer_tgt = get_or_build_tokenizer(self.config, self.ds_raw, self.config['lang_tgt'])
         self.model = get_model(self.config, self.tokenizer_src.get_vocab_size(), self.tokenizer_tgt.get_vocab_size())
